5472acc2a9ce4e6b7079d1838e8d279cd73b1e4f
Giomar <121203604+GiomarMC@users.noreply.github.com>
2023-06-28 12:09:15 -0500

    cambios en caracter view y sprite (#3)
    Co-authored-by: AnLu996 <coar.andreacuela@gmail.com>

--- coding/character_view.h
+++ coding/character_view.h
@@ -9,11 +9,14 @@ class Character_view
     private:
         Sprite sprite;
         float posX, posY;
+        sf::Keyboard::Key ultimatecla;
 
     public:
         Character_view(const std::string& archivo, float x, float y);
         void setPosition(float x, float y);
         void draw(sf::RenderWindow& window);
+        void moveCharacterLeft();
+        void moveCharacterRight();
 };
 
 #endif
--- coding/sprite.h
+++ coding/sprite.h
@@ -2,6 +2,7 @@
 #define SPRITE_H
 
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 class Sprite
 {
@@ -9,8 +10,14 @@ class Sprite
         sf::Texture texture;
         sf::Sprite sprite;
         sf::Vector2f center, cambio, tamano;
+        int alto = 81;
+        int ancho = 115;
+        
+
 
     public:
+        int indiceX = 0;
+        int indiceY = 0;
         Sprite(const std::string& archivo, float x, float y);
         void setPosition(float x, float y);
         void setScale(float scaleX, float scaleY);
